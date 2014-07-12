c1f348a5106b1df73820051cb68759f5209be64b
Andreas Buttenschoen <andreas.buttenschoen@ualberta.ca>
2014-07-12 17:56:44 -0600

    Remove unneeded stuff

--- include/debug.h
+++ include/debug.h
@@ -2,6 +2,9 @@
 #define DEBUG_H
 
 #ifdef DEBUG_BUILD
+
+#include <iostream>
+
 #define DEBUG(x) do { std::cerr << "File:"  <<  __FILE__ << ":" << __LINE__ << ": " << x; } while (0)
 #define DEBUG2(x) do { std::cerr << "File:" << __FILE__ << ":" << __LINE__ << ": " << #x << "=" << x << std::endl; } while (0)
 
@@ -13,68 +16,6 @@
 #define ASSERT(x) assert(x);
 
 #define PRINT_FARGS std::printf("File:%s:%d: %s\n",__FILE__,__LINE__, __PRETTY_FUNCTION__);
-
-// Note from c-template-name-pretty-prin
-// stackoverflow
-#include <typeinfo>
-#include <string>
-#include <iostream>
-#include <cxxabi.h>
-#define TYPENAME(TYPE) typeid_name(typeid(TYPE).name())
-std::string indent(std::string str, const std::string &indent = "  ") {
-    std::string indent_ = std::string("\n");
-    size_t token = 0;
-
-    bool one_line = false;
-    while ((token = str.find_first_of("<>,", token)) != std::string::npos) {
-        size_t size = str.size();
-        size_t close, open, comma;
-
-        switch(str[token]) {
-        case '<':
-            close = str.find(">", token+1);
-            open = str.find("<", token+1);
-            comma = str.find(",", token+1);
-            one_line = !(close > open) && !(comma < close);
-
-            if (one_line) break;
-            indent_.append(indent);
-
-        case ',':
-            str.insert(token + 1, indent_);
-            break;
-
-        case '>':
-            if (!one_line) {
-                indent_.erase(indent_.size() - indent.size());
-                str.insert(token, indent_);
-            }
-            one_line = false;
-        }
-
-        token += 1 + str.size() - size;
-
-        const size_t nw = str.find_first_not_of(" ", token);
-        if(nw != std::string::npos) {
-            str.erase(token, nw-token);
-        }
-    }
-
-    return str;
-}
-std::string typeid_name(const char* name) {
-// #ifdef HAVE_CXA_DEMANGLE
-    size_t size;
-    int status;
-    char *buf = abi::__cxa_demangle(name, NULL, &size, &status);
-    if (status  != 0) throw status;
-    std::string string(buf);
-    free(buf);
-    return indent(string);
-// #else
-//     return name;
-// #endif
-}
 
 #else
 #define NDEBUG
