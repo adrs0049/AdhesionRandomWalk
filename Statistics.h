bd762badb1c9eeacb9ea5c9e2ce21c1615d22628
Andreas Buttenschoen <andreas.buttenschoen@ualberta.ca>
2014-07-13 23:20:08 -0600

    Add a statistics class that fits better to the RandomWalk target

--- Statistics.h
+++ Statistics.h
@@ -3,9 +3,15 @@
 #ifndef STATISTICS_H
 #define STATISTICS_H
 
-#include "array.h"
+#include <valarray>
+#include <wrapper.h>
 #include <type_traits>
+#include <debug.h>
+#include <cassert>
 
+#define ARRAY(x) std::valarray<double> {x}
+
+template<class T>
 class StatisticsBase
 {
 public:
@@ -13,26 +19,142 @@ public:
 	
 	StatisticsBase() {}
 	
-	virtual void DumpOneResult(const float_type result) = 0;
-	virtual std::vector<std::vector<float_type> > GetResultsSoFar() const = 0;
+	virtual void DumpOneResult(const std::valarray<T>& result) = 0;
+	virtual void DumpOneResult(const std::valarray<T>&& result) = 0;
+	virtual std::valarray<std::valarray<float_type> > GetResultsSoFar() const = 0;
 	virtual StatisticsBase* clone() const = 0;
 	virtual ~StatisticsBase() {}
 private:
 };
 
-class StatisticsMean : public StatisticsBase
+template<class T>
+class StatisticsMeanImpl : public StatisticsBase<T>
 {
 public:
-	StatisticsMean();
+	StatisticsMeanImpl();
 	
-	virtual void DumpOneResult(const float_type result);
-	virtual std::vector<std::vector<float_type> > GetResultsSoFar() const;
-	virtual StatisticsMean* clone() const;
-	virtual ~StatisticsMean() {}
+	virtual void DumpOneResult(const std::valarray<T>& result);
+	virtual void DumpOneResult(const std::valarray<T>&& result);
+	virtual std::valarray<std::valarray<double> > GetResultsSoFar() const;
+	virtual StatisticsMeanImpl* clone() const;
+	virtual ~StatisticsMeanImpl() {}
 private:
-	float_type RunningSum;
-	float_type RunningSumSquare;
+	T RunningSum;
+	T RunningSumSquare;
 	unsigned long PathsDone;
 };
+
+template<class T>
+class StatisticsRWImpl : public StatisticsBase<T>
+{
+public:
+    StatisticsRWImpl();
+    
+    virtual void DumpOneResult(const std::valarray<T>& result);
+    virtual void DumpOneResult(const std::valarray<T>&& result);
+	virtual std::valarray<std::valarray<double>> GetResultsSoFar() const;
+	virtual StatisticsRWImpl* clone() const;
+	virtual ~StatisticsRWImpl() {}
+	
+	void resize(unsigned long outer, unsigned long inner);
+private:
+	std::valarray<std::valarray<T>> results;
+	unsigned long PathsDone;
+};
+
+template<class T>
+StatisticsRWImpl<T>::StatisticsRWImpl()
+: results(1), PathsDone(0UL)
+{}
+
+template<class T>
+void StatisticsRWImpl<T>::resize(long unsigned int outer, long unsigned int inner)
+{
+	results.resize(outer);
+	for (auto& ivec : results)
+		ivec.resize(inner);
+}
+
+template<class T>
+void StatisticsRWImpl<T>::DumpOneResult(const std::valarray< T >& result)
+{
+	assert(PathsDone<results.size());
+ 	results[PathsDone] = result;
+	PathsDone++;
+}
+
+template<class T>
+void StatisticsRWImpl<T>::DumpOneResult(const std::valarray< T >&& result)
+{
+	DEBUG2(results.size());
+	assert(PathsDone<results.size());
+	results[PathsDone] = result;
+	PathsDone++;
+}
+
+template<class T>
+StatisticsRWImpl<T>* StatisticsRWImpl<T>::clone() const
+{
+	return new StatisticsRWImpl(*this);
+}
+
+template<class T>
+std::valarray< std::valarray< double > > StatisticsRWImpl<T>::GetResultsSoFar() const
+{
+	return results;
+}
+
+template<class T>
+StatisticsMeanImpl<T>::StatisticsMeanImpl()
+: RunningSum(0.0), 
+RunningSumSquare(0.0),
+PathsDone(0UL)
+{}
+
+template<class T>
+StatisticsMeanImpl<T>* StatisticsMeanImpl<T>::clone() const
+{
+	return new StatisticsMeanImpl(*this);
+}
+
+template<class T>
+void StatisticsMeanImpl<T>::DumpOneResult(const std::valarray<T>& result)
+{
+	PathsDone++;
+	RunningSum+=result[0];
+	RunningSumSquare+=(result[0]*result[0]);
+}
+
+template<class T>
+void StatisticsMeanImpl<T>::DumpOneResult(const std::valarray<T>&& result)
+{
+	PathsDone++;
+	RunningSum+=result[0];
+	RunningSumSquare+=(result[0]*result[0]);
+}
+
+/*
+ * GetResultsSoFar returns a vector<vector>
+ * At position (0,0) the mean is returned
+ * At position (0,1) the variance is returned
+ * 
+ */
+template<class T>
+std::valarray< std::valarray< double > > StatisticsMeanImpl<T>::GetResultsSoFar() const
+{
+	double xaverage = RunningSum / PathsDone;
+	double x2average = RunningSumSquare / PathsDone;
+	
+	std::valarray<std::valarray<double>> Results(1);
+	Results[0].resize(2);
+	Results[0][0] = xaverage;
+	Results[0][1] = x2average - xaverage * xaverage;
+	
+	return Results; 
+}
+
+// usefule alias
+using StatisticsMean = StatisticsMeanImpl<double>;
+using StatisticsRW = StatisticsRWImpl<double>;
 
 #endif
--- Statistics_test.cpp
+++ Statistics_test.cpp
@@ -4,6 +4,9 @@
 // redefine private to public allows unit testing of private stuff
 #define private public
 #include "Statistics.h"
+#include <iostream>
+#include <iomanip>
+#include <debug.h>
 
 #define ITERATIONS 100
 
@@ -18,7 +21,7 @@ BOOST_AUTO_TEST_CASE( constructor_test )
 	BOOST_CHECK_EQUAL(new_test->PathsDone, 0);
 }
 
-BOOST_AUTO_TEST_CASE( random_number_generator_test )
+BOOST_AUTO_TEST_CASE( Statistics_Mean_test )
 {
 	const double tol = 1E-6;
 	
@@ -27,11 +30,34 @@ BOOST_AUTO_TEST_CASE( random_number_generator_test )
 	BOOST_CHECK_EQUAL(test.PathsDone, 0);
 	
 	for (unsigned long i = 0; i < ITERATIONS; i++)
-		test.DumpOneResult(i);
+		test.DumpOneResult(ARRAY(i));
 	
 	BOOST_CHECK_EQUAL(test.RunningSum, 4950.0);
 	BOOST_CHECK_EQUAL(test.PathsDone, 100);
 	
-	std::vector<std::vector<double>> results = test.GetResultsSoFar();
+	std::valarray<std::valarray<double>> results = test.GetResultsSoFar();
 	BOOST_CHECK_CLOSE(49.50, results[0][0], tol);
+}
+
+BOOST_AUTO_TEST_CASE( Random_walk_statistics_test )
+{
+	const double tol = 1E-6;
+	
+	StatisticsRW test;
+	BOOST_CHECK_EQUAL(test.results.size(), 1);
+	BOOST_CHECK_EQUAL(test.PathsDone, 0);
+	
+	test.resize(ITERATIONS, 6);
+	BOOST_CHECK_EQUAL(test.results.size(), ITERATIONS);
+	BOOST_CHECK_EQUAL(test.PathsDone, 0);
+
+	for (unsigned long i = 0; i < ITERATIONS; i++)
+		test.DumpOneResult({1,1,1,1,1,1});
+	
+ 	BOOST_CHECK_EQUAL(test.PathsDone, 100);
+	
+ 	std::valarray<std::valarray<double>> results = test.GetResultsSoFar();
+	for (auto& ivec : results) 
+            for (auto elem : ivec) 
+                BOOST_CHECK_CLOSE(1.0, elem, tol);
 }
