e203fe7f816a6475c8fe9bbcab696dfecb25f77e
Andreas Buttenschoen <andreas.buttenschoen@ualberta.ca>
2014-07-17 02:21:36 -0600

    Lots of hacks to get data back from this

--- RandomWalkStatistics.cpp
+++ RandomWalkStatistics.cpp
@@ -2,13 +2,26 @@
 
 #include "RandomWalkStatistics.h"
 
-void RandomWalkStatistics::DumpOneResult(std::unique_ptr<DataBase> d)
+void RandomWalkStatistics::DumpOneResult(std::shared_ptr<DataBase> d)
 {
 	data.emplace_back(std::move(d));
 }
 
+DataBase * RandomWalkStatistics::GetResultsSoFar() const
+{
+	for (const auto& elem : data)
+	{
+		elem->getData();
+	}
+}
+
+DataBase * RandomWalkStatistics::GetResultsAtPos(const unsigned long pos) const
+{
+	return data.at(pos).get();
+}
+
 // FIXME
-void RandomWalkStatistics::GetResultsSoFar() const
+void RandomWalkStatistics::PrintResultsSoFar() const
 {
 	unsigned int run {1U};
 	for (const auto& elem : data) {
@@ -22,12 +35,22 @@ std::size_t RandomWalkStatistics::GetSize() const
 	return data.size();
 }
 
-std::size_t RandomWalkStatistics::GetElementSize() const
+std::size_t RandomWalkStatistics::GetElementSizeMax() const
 {
+std::size_t sz {0UL};
+	for (const auto& elem : data)
+		if (sz < elem->size())
+			sz = elem->size();
+	return sz;
+}
+
+std::size_t RandomWalkStatistics::GetElementSize(const unsigned long pos) const
+{
+	/*
 	std::size_t sz {0UL};
 	for (const auto& elem : data)
 		if (sz < elem->size())
 			sz = elem->size();
-
-	return sz;
+	*/
+	return data.at(pos)->size();
 }
--- RandomWalkStatistics.h
+++ RandomWalkStatistics.h
@@ -20,29 +20,63 @@ public:
 	~RandomWalkStatistics() {}
 	
 	// FIXME
-	virtual void DumpOneResult(std::unique_ptr<DataBase> d);
-	virtual void GetResultsSoFar() const;
+	virtual void DumpOneResult(std::shared_ptr<DataBase> d);
+	virtual void PrintResultsSoFar() const;
+	virtual DataBase * GetResultsSoFar() const;
+	virtual DataBase * GetResultsAtPos(const unsigned long pos) const;
 	
 	// size functions
 	virtual std::size_t GetSize() const;
-	virtual std::size_t GetElementSize() const;
-	
+	virtual std::size_t GetElementSize(const unsigned long pos) const;
+	virtual std::size_t GetElementSizeMax() const;
 private:
-	std::vector<std::unique_ptr<DataBase> > data;
+	std::vector<std::shared_ptr<DataBase> > data;
 };
 
 #ifdef USE_C_INTERFACE
 // C Interface for python usage
 extern "C"
 {
-	RandomWalkStatistics* new_RandomWalkStatistics()
+	RandomWalkStatistics* new_RandomWalkStatistics(double p[][2], std::size_t dim)
 	{
-		return new RandomWalkStatistics();
+		std::vector<std::pair<long, unsigned long>> res;
+		for (unsigned long i = 0; i < dim; ++i)
+			res.push_back(std::make_pair(p[i][0],p[i][1]));
+		
+		for (unsigned long i = 0; i < dim; ++i)
+			res.push_back(std::make_pair(p[i][0],p[i][1]));
+		
+		for (unsigned long i = 0; i < dim; ++i)
+			res.push_back(std::make_pair(p[i][0],p[i][1]));
+		
+		auto ptr = new RandomWalkStatistics();
+		ptr->DumpOneResult(std::shared_ptr<DataBase> (new RandomWalkData(std::move(res))));
+		return ptr;
+		
+// 		return new RandomWalkStatistics();
 	}
 	
-	void RandomWalkStatistics_getData(RandomWalkStatistics* stat)
+	RandomWalkStatistics* new2_RandomWalkStatistics()
 	{
-		stat->GetResultsSoFar();
+		std::vector<std::pair<long, unsigned long>> res;
+		for (unsigned long i = 0; i < 5; ++i)
+			for (unsigned long j = 0; j < 5; ++j)
+				res.push_back(std::make_pair(i,j));
+		
+		auto ptr = new RandomWalkStatistics();
+		ptr->DumpOneResult(std::shared_ptr<DataBase> (new RandomWalkData(std::move(res))));
+		return ptr;
+	}
+	
+	void * RandomWalkStatistics_getData(RandomWalkStatistics* stat, 
+										const unsigned long pos)
+	{
+		return stat->GetResultsAtPos(pos);
+	}
+	
+	void RandomWalkStatistics_printData(RandomWalkStatistics* stat)
+	{
+		stat->PrintResultsSoFar();
 	}
 	
 	// ATTENTION NO IDEA IF THIS WORKS AND IS NEEDED
@@ -58,9 +92,17 @@ extern "C"
 		return stat->GetSize();
 	}
 	
-	std::size_t RandomWalkStatistics_getElementSize(RandomWalkStatistics * stat)
+	// get vector element size at a location
+	std::size_t RandomWalkStatistics_getElementSize(RandomWalkStatistics * stat, 
+													const unsigned long pos)
 	{
-		return stat->GetElementSize();
+		return stat->GetElementSize(pos);
+	}
+	
+	// get vector element size at a location
+	std::size_t RandomWalkStatistics_getElementSizeMax(RandomWalkStatistics * stat)
+	{
+		return stat->GetElementSizeMax();
 	}
 }
 #endif
