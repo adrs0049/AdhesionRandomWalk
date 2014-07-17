ac68fddeaa93e9b6300b38fb4f7b9c8666a29c19
Andreas Buttenschoen <andreas.buttenschoen@ualberta.ca>
2014-07-16 20:18:27 -0600

    RandomWalkStatistics add python interface

--- RandomWalkStatistics.h
+++ RandomWalkStatistics.h
@@ -31,4 +31,38 @@ private:
 	std::vector<std::unique_ptr<DataBase> > data;
 };
 
+#ifdef USE_C_INTERFACE
+// C Interface for python usage
+extern "C"
+{
+	RandomWalkStatistics* new_RandomWalkStatistics()
+	{
+		return new RandomWalkStatistics();
+	}
+	
+	void RandomWalkStatistics_getData(RandomWalkStatistics* stat)
+	{
+		stat->GetResultsSoFar();
+	}
+	
+	// ATTENTION NO IDEA IF THIS WORKS AND IS NEEDED
+	/*
+	void RandomWalkStatistics_DumpData(RandomWalkStatistics * stat, DataBase * d)
+	{
+		stat->DumpOneResult(std::unique_ptr<DataBase>(nullptr));
+	}
+	*/
+	
+	std::size_t RandomWalkStatistics_getSize(RandomWalkStatistics * stat)
+	{
+		return stat->GetSize();
+	}
+	
+	std::size_t RandomWalkStatistics_getElementSize(RandomWalkStatistics * stat)
+	{
+		return stat->GetElementSize();
+	}
+}
+#endif
+
 #endif
