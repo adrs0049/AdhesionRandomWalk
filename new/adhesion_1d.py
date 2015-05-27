240f380905e5aa774bcb7818eb6551f31fd8f250
Andreas Buttenschoen <andreas.buttenschoen@ualberta.ca>
2015-05-26 22:40:11 -0600

    python script add some debugging help

--- new/adhesion_1d.py
+++ new/adhesion_1d.py
@@ -18,7 +18,7 @@ class Player(object):
         # domain size, domain is [-L, L]
         self.L = 5 
         # step size 
-        self.h = 0.1
+        self.h = 0.05
         self.Dt = 0.005
         # final time
         self.Ft = 0.1
@@ -93,36 +93,77 @@ class Player(object):
 
         # get diffusion soln
         xd, u = self.computeDiffusionSoln()
-
-        #hist, bin_edges = np.histogram(x, bins=100, density=True)
-        #bins = np.arange(-self.L, self.L +
-        #    self.h, bar_width), density=True)
+        
+        # plotting
         plt.bar(bins[:-1], x, width=bar_width)
         plt.plot(xd, u, color='k')
 
+        # plot error
+        error = u - x
+        plt.plot(xd, error, color='r')
+
+        print('shape u=', np.shape(u))
+        print('shape x=', np.shape(x))
+        self.print_error(u, x)
+    
         #plt.bar(x)
         #ax.xlim(min(bins), max(bins))
+        plt.show()
+
+    def plotIC(self):
+        bar_width = self.h
+        x=self.getCellPath()
+        bins=np.arange(-self.L, self.L + self.h, bar_width)
+
+        print('shape bins=', np.shape(bins))
+        print('shape x=', np.shape(x))
+
+        # get diffusion soln
+        xd, u = self.computeDiffusionSoln(FinalTime=0.0)
+        
+        # plotting
+        plt.bar(bins[:-1], x, width=bar_width)
+        plt.plot(xd, u, color='k')
+
+        # plot error
+        error = u - x
+        plt.plot(xd, error, color='r')
+
+        print('shape u=', np.shape(u))
+        print('shape x=', np.shape(x))
+        self.print_error(u, x)
+
+        plt.ylim(0,1000)
         plt.show()
 
     def setupSim(self, NoPlayers=100, NoSteps=1000):
         self.updateValues(NoPlayers, NoSteps)
         self.sim = s.Simulator(self.param)
 
-    def computeDiffusionSoln(self):
+    def computeDiffusionSoln(self, FinalTime=None):
         N=self.getDomainSizeL()
         Nhalf = N/2
-        T=self.getFinalTime()
 
+        if not FinalTime==None:
+            T = FinalTime
+        else:
+            T=self.getFinalTime()
+        
+        print('Solving heat equation using the FFT')
         print('N=',N,' T=',T)
 
         # domain is [-L, L]
-        xd=np.linspace(self.getDomainLeftBoundary(), self.getDomainRightBoundary(), N+1)
-        u0=np.zeros(N+1)
+        xd=np.linspace(self.getDomainLeftBoundary(), self.getDomainRightBoundary(), N)
+        u0=np.zeros(N)
         u0[N/2]=self.NoPlayers/2.0
         u0[N/2+1]=self.NoPlayers/2.0
+
+        if FinalTime==0.0:
+            return xd, u0
+
         # IC
         F = np.fft.fft(u0)
-        k = np.append(np.arange(Nhalf+1), np.arange(-Nhalf,0,1))
+        k = np.append(np.arange(Nhalf), np.arange(-Nhalf,0,1))
         k2 = self.getDiffusionCoeff() * k**2
         W=np.exp(-k2 * T)
         Ft = F * W
@@ -130,14 +171,23 @@ class Player(object):
 
         return xd, u
 
+    # Compute norm between vectors x, y
+    def norm1(self, x, y):
+        return np.linalg.norm(x-y, ord=1)
+
+    # compute l2 between vectors x, y
+    def norm2(self, x, y):
+        return np.linalg.norm(x-y, ord=2)
+
+    def print_error(self, x, y):
+        print('The L1 error=', self.norm1(x, y))
+        print('The L2 error=', self.norm2(x, y))
+
     def printSim(self):
         self.sim._print()
 
-    def runSimulation(self, NoPlayers=10000, NoSteps=1000):
-        assert (NoPlayers>0), "Number of players has to be larger 0"
-        assert (NoSteps>0), "Number of steps has to be larger 0"
-        if NoSteps>1E6: print ('WARNING number of steps is very large!!')
-
+    def prepareSimulation(self, NoPlayers, NoSteps):
+ 
         self.NoPlayers = NoPlayers
 
         # make sure parameters are up to date
@@ -148,11 +198,16 @@ class Player(object):
         self.sim._print()
         time.sleep(1)
 
-        #self.doHistogram()
+    def runSimulation(self, NoPlayers=500, NoSteps=1000):
+        assert (NoPlayers>0), "Number of players has to be larger 0"
+        assert (NoSteps>0), "Number of steps has to be larger 0"
+        if NoSteps>1E6: print ('WARNING number of steps is very large!!')
+
+        self.prepareSimulation(NoPlayers, NoSteps)
 
         # run the simulation
         self.sim.run()
 
 if __name__ == '__main__':
     player = Player()
-    player.runSimulation()
+    #player.runSimulation()
