695b04c8c5f7ec8577fd4281689340de7fd73114
Andreas Buttenschoen <andreas.buttenschoen@gmail.com>
2015-07-01 21:07:58 -0600

    add support for longer time periods to chronos

--- utils/chronos.h
+++ utils/chronos.h
@@ -1,9 +1,9 @@
 /*
  * chronos.h
- * 
- * Andreas Buttenschoen 
+ *
+ * Andreas Buttenschoen
  * 2014
- * 
+ *
  */
 
 #ifndef CHRONOS_H
@@ -19,6 +19,8 @@
 
 using std::chrono::high_resolution_clock;
 using std::chrono::duration_cast;
+using std::chrono::hours;
+using std::chrono::minutes;
 using std::chrono::seconds;
 using std::chrono::milliseconds;
 using std::chrono::microseconds;
@@ -28,11 +30,15 @@ namespace chronos {
 
 namespace detail {
 
-enum class UNIT : unsigned short { S, MS, US, NS };
+enum class UNIT : unsigned short { HR, MIN, S, MS, US, NS };
 
 std::string get_unit(UNIT unit)
 {
 	switch (unit) {
+        case UNIT::HR:
+            return {" (hr)"};
+        case UNIT::MIN:
+            return {" (min)"};
 		case UNIT::S:
 			return {" (s)"};
 		case UNIT::MS:
@@ -53,47 +59,59 @@ public:
 	Chronos(std::ostream & m_os)
 	: start(high_resolution_clock::now()), os(m_os)
 	{}
-	
+
 	Chronos()
 	: Chronos(std::cout)
 	{}
-	
-	~Chronos() 
+
+	~Chronos()
 	{
 		auto end = high_resolution_clock::now();
-		
-		auto ds = duration_cast<seconds>(end-start);
+
+        auto dhours = duration_cast<hours>(end-start);
+        auto dmin = duration_cast<minutes>(end-start-dhours);
+		auto ds = duration_cast<seconds>(end-start-dmin);
 		auto dms = duration_cast<milliseconds>(end-start-ds);
 		auto dus = duration_cast<microseconds>(end-start-ds-dms);
 		auto dns = duration_cast<nanoseconds>(end-start-ds-dms-dus);
-		
+
 		std::ostringstream ostr;
 		detail::UNIT unit;
-		
+
 		ostr << "Program Execution Time: ";
-		if (ds.count()!=0) {
+		if (dhours.count()!=0) {
+            unit=detail::UNIT::HR;
+            ostr << dhours.count() << ":";
+            ostr << dmin.count() << ":";
+            ostr << ds.count();
+        } else if (dmin.count()!=0) {
+            unit=detail::UNIT::MIN;
+            ostr << dmin.count() << ":";
+            ostr << ds.count();
+        } else if (ds.count()!=0) {
 			unit=detail::UNIT::S;
 			ostr << ds.count() << ".";
-			ostr << dms.count() << ".";
-			ostr << dus.count() << ".";
+			ostr << dms.count();
 		} else if (dms.count()!=0) {
 			unit=detail::UNIT::MS;
 			ostr << dms.count() << ".";
-			ostr << dus.count() << ".";
+			ostr << dus.count();
 		} else if (dus.count()!=0) {
 			unit=detail::UNIT::US;
 			ostr << dus.count() << ".";
-		} else {
+	        ostr << dns.count();
+        } else {
 			unit=detail::UNIT::NS;
-		}
-		ostr << dns.count();
-		ostr << detail::get_unit(unit);
+		    ostr << dns.count();
+        }
+
+        ostr << detail::get_unit(unit);
 		ostr << std::endl;
-		
+
 		os << ostr.str();
 	}
-	
-private:	
+
+private:
 	high_resolution_clock::time_point start;
 	std::ostream & os;
 };
