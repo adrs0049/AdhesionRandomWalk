1bca50218c1cd3d0f4b25850877a35ce74193764
Andreas Buttenschoen <andreas.buttenschoen@gmail.com>
2015-07-08 23:23:07 -0600

    inherit Pycallable from listener class

--- tmp/PyCallback.h
+++ tmp/PyCallback.h
@@ -2,17 +2,22 @@
 #define PYCALLBACK_H
 
 #include "Event.h"
-#include <iostream>
+#include "EventListener.h"
 
+#include <iostream>
 #include<Python.h>
 
-template<class E>
-class PyCallback
-{
-    PyObject * func;
-    PyCallback& operator=(const PyCallback&) = delete;
+// E = PyObject*
 
+template<typename E>
+class PyCallback : public AbstractEventListener
+{
 public:
+    PyCallback() {}
+
+    // STATIC_ASSERT that E is of type pointer
+
+    /*
     PyCallback(const PyCallback& o)
         : func(o.func)
     {
@@ -20,24 +25,58 @@ public:
         Py_XDECREF(o.func);
         Py_XINCREF(this->func);
     }
+    */
 
-    PyCallback(PyObject * _func)
-        : func(_func)
+    PyCallback(Event<E>* e, int listenerId)
+        : AbstractEventListener(e, listenerId)
     {
         Py_XINCREF(this->func);
         assert(PyCallable_Check(this->func));
     }
 
-    PyCallback()
-        : func(nullptr)
-    {}
+    PyCallback& operator=(const PyCallback&) = delete;
+    PyCallback(const PyCallback& other) = delete;
+
+    PyCallback(PyCallback&& other)
+    {
+        listenerId = other.listenerId;
+        ae = other.ae;
+        func = other.func;
+
+        // python required functions
+        Py_XDECREF(other.func);
+        Py_XINCREF(this->func);
+
+        // unset everything in other
+        other.func = nullptr;
+        other.ae = nullptr;
+    }
+
+    PyCallback& operator=(PyCallback&& rhs)
+    {
+        if (this != &rhs)
+        {
+            destroyListener();
+            listenerId = rhs.listenerId;
+            ae = rhs.ae;
+            func = rhs.func;
+
+            Py_XDECREF(rhs.func);
+            Py_XINCREF(this->func);
+
+            rhs.ae = nullptr;
+            rhs.func = nullptr;
+        }
+        return *this;
+    }
 
     ~PyCallback()
     {
         Py_XDECREF(this->func);
     }
 
-    void operator() (const E& e)
+    // the call function
+    void operator() (const std::string& s)
     {
         if(func == nullptr || Py_None == func || !PyCallable_Check(func))
             return;
@@ -46,6 +85,9 @@ public:
         Py_DECREF(args);
         Py_XDECREF(result);
     }
+
+private:
+    E func = nullptr;
 };
 
 #endif
