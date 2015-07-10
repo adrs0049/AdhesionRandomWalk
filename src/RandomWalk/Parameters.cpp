1fb83567fd467ace5e62bc42587363e7cb9dc914
Andreas Buttenschoen <andreas.buttenschoen@gmail.com>
2015-07-10 16:36:30 -0600

    split some of the functions into a separate cpp file

---
+++ src/RandomWalk/Parameters.cpp
@@ -0,0 +1,107 @@
+
+#include "Parameters.h"
+#include "debug.h"
+
+Parameters::Parameters() {}
+
+Parameters::Parameters(double Dsize_, double StepSize_,
+        double FinalTime_, unsigned long _ic_p)
+: DomainSize(Dsize_), DomainSizeL(0),
+    StepSize(StepSize_), FinalTimes(1, FinalTime_),
+    ic_p(_ic_p)
+{
+    // TODO check if this is correct
+    domainShape.resize(2, 0);
+    domainShape[0] = -DomainSize/2;
+    domainShape[1] = DomainSize/2;
+
+    NumberOfCells = 0;
+
+    update();
+    print_info();
+}
+
+Parameters::Parameters(double Dsize_, double StepSize_,
+        std::vector<double> FinalTimes_, unsigned long _ic_p)
+: DomainSize(Dsize_), DomainSizeL(0),
+    StepSize(StepSize_), FinalTimes(FinalTimes_),
+    ic_p(_ic_p)
+{
+    // TODO check if this is correct
+    domainShape.resize(2, 0);
+    domainShape[0] = -DomainSize/2;
+    domainShape[1] = DomainSize/2;
+
+    NumberOfCells = 0;
+
+    update();
+    print_info();
+}
+
+Parameters::Parameters(std::vector<double> _shape, double _stepSize,
+        double _finalTime,
+        unsigned long _ic_p)
+: domainShape(_shape), StepSize(_stepSize), FinalTimes(1, _finalTime),
+    ic_p(_ic_p)
+{
+    ASSERT(_shape.size()==2, "shape size is invalid!");
+
+    DomainSize = domainShape[1] - domainShape[0];
+
+    update();
+    print_info();
+}
+
+Parameters::Parameters(std::vector<double> _shape, double _stepSize,
+        std::vector<double> FinalTimes_,
+        unsigned long _ic_p)
+: domainShape(_shape), StepSize(_stepSize),
+    FinalTimes(FinalTimes_), ic_p(_ic_p)
+{
+    ASSERT(_shape.size()==2, "shape size is invalid!");
+
+    DomainSize = domainShape[1] - domainShape[0];
+
+    update();
+    print_info();
+}
+
+void Parameters::print_info()
+{
+    Check();
+    std::cout << "Domain Size:" << DomainSize;
+    std::cout << " Domain Size L:" << DomainSizeL;
+    std::cout << " Domain = (" << domainShape[0] << ", "
+        << domainShape[1] << ").";
+    std::cout << " StepSize:" << StepSize << std::endl;
+    std::cout << " Sensing Radius:" << SensingRadiusL;
+    std::cout << " FinalTimes=(";
+    std::copy(FinalTimes.begin(), FinalTimes.end(),
+            std::ostream_iterator<double>(std::cout, ", "));
+    std::cout << ")" << std::endl;
+}
+
+void Parameters::update()
+{
+    // sort elements of FinalTimes, required!!
+    std::sort(FinalTimes.begin(), FinalTimes.end());
+    DomainSizeL = DomainSize / StepSize;
+    SensingRadiusL = SensingRadius / StepSize;
+    lambda = 1E2;
+    set = true;
+
+    Check();
+}
+
+void Parameters::Check()
+{
+    ASSERT(FinalTimes.size()>0, "FinalTimes are not set");
+    ASSERT(set, "Parameters are not initalized.");
+    ASSERT(domainShape.size()==2, "Domain shape has invalid size!");
+
+    ASSERT(SensingRadius < DomainSize/2, "SensingRadius is too large!");
+
+    ASSERT(ic_p!=0, "Initial number of cells can't be zero!");
+    ASSERT(0.0<=InitCellDensity && InitCellDensity<=1.0, "Initial Cell Density " << InitCellDensity << " is invalid. The valid range is [0,1].");
+    ASSERT(Diffusion>=0.0, "The diffusion coefficient can't be negative");
+}
--- src/RandomWalk/Parameters.h
+++ src/RandomWalk/Parameters.h
@@ -5,100 +5,27 @@
 #include <vector>
 #include <iterator>
 #include <algorithm>
-#include "debug.h"
 
 class Parameters
 {
 	public:
-		Parameters() {}
+		Parameters();
 		Parameters(double Dsize_, double StepSize_,
-				double FinalTime_, unsigned long _ic_p)
-			: DomainSize(Dsize_), DomainSizeL(0),
-			StepSize(StepSize_), FinalTimes(1, FinalTime_),
-			ic_p(_ic_p)
-	{
-		// TODO check if this is correct
-		domainShape.resize(2, 0);
-		domainShape[0] = -DomainSize/2;
-		domainShape[1] = DomainSize/2;
-
-		NumberOfCells = 0;
-
-		update();
-		print_info();
-	}
+				double FinalTime_, unsigned long _ic_p);
 
         Parameters(double Dsize_, double StepSize_,
-				std::vector<double>& FinalTimes_, unsigned long _ic_p)
-			: DomainSize(Dsize_), DomainSizeL(0),
-			StepSize(StepSize_), FinalTimes(FinalTimes_),
-			ic_p(_ic_p)
-	{
-		// TODO check if this is correct
-		domainShape.resize(2, 0);
-		domainShape[0] = -DomainSize/2;
-		domainShape[1] = DomainSize/2;
-
-		NumberOfCells = 0;
-
-		update();
-		print_info();
-	}
+				std::vector<double> FinalTimes_, unsigned long _ic_p);
 
 		Parameters(std::vector<double> _shape, double _stepSize,
-				double _finalTime,
-				unsigned long _ic_p)
-			: domainShape(_shape), StepSize(_stepSize), FinalTimes(1, _finalTime),
-			ic_p(_ic_p)
-	{
-		ASSERT(_shape.size()==2, "shape size is invalid!");
+				double _finalTime, unsigned long _ic_p);
 
-		DomainSize = domainShape[1] - domainShape[0];
+		Parameters(std::vector<double> _shape, double _stepSize,
+				std::vector<double> FinalTimes_,
+				unsigned long _ic_p);
 
-		update();
-		set = true;
-		print_info();
-	}
-
-		void print_info()
-		{
-			Check();
-			std::cout << "Domain Size:" << DomainSize;
-			std::cout << " Domain Size L:" << DomainSizeL;
-			std::cout << " Domain = (" << domainShape[0] << ", "
-				<< domainShape[1] << ").";
-			std::cout << " StepSize:" << StepSize << std::endl;
-			std::cout << " Sensing Radius:" << SensingRadiusL;
-			std::cout << " FinalTimes=(";
-			std::copy(FinalTimes.begin(), FinalTimes.end(),
-					  std::ostream_iterator<double>(std::cout, ", "));
-			std::cout << ")" << std::endl;
-		}
-
-		void update()
-		{
-			// sort elements of FinalTimes, required!!
-			std::sort(FinalTimes.begin(), FinalTimes.end());
-			DomainSizeL = DomainSize / StepSize;
-			SensingRadiusL = SensingRadius / StepSize;
-			lambda = 1E2;
-			set = true;
-
-			Check();
-		}
-
-		void Check()
-		{
-			ASSERT(FinalTimes.size()>0, "FinalTimes are not set");
-			ASSERT(set, "Parameters are not initalized.");
-			ASSERT(domainShape.size()==2, "Domain shape has invalid size!");
-
-			ASSERT(SensingRadius < DomainSize/2, "SensingRadius is too large!");
-
-			ASSERT(ic_p!=0, "Initial number of cells can't be zero!");
-			ASSERT(0.0<=InitCellDensity && InitCellDensity<=1.0, "Initial Cell Density " << InitCellDensity << " is invalid. The valid range is [0,1].");
-			ASSERT(Diffusion>=0.0, "The diffusion coefficient can't be negative");
-		}
+		void print_info();
+		void update();
+		void Check();
 
 		const std::vector<double>& getFinalTimes() const { return FinalTimes; }
 
@@ -118,7 +45,8 @@ class Parameters
 		const double getDriftSim() const
 		{ return getDrift() / ( 2.0 * getLambda() * getStepSizeSquare()); }
 		const unsigned long getICp() { return ic_p; }
-		unsigned long getSteps() { return steps; }
+		const unsigned long getSteps() const { return steps; }
+		const double getCurrentTime() const { return time; }
 
 		void setDiffusion(double _Diffusion) { Diffusion = _Diffusion; }
 		void setNumberOfCells(long number) { NumberOfCells = number; }
@@ -127,9 +55,10 @@ class Parameters
 		void setDiscreteX(double _h) { StepSize = _h; }
 		void setLambda(double _l) { lambda = _l; }
 		void setSteps(unsigned long _steps) { steps = _steps; }
+		void setCurrentTime(const double _time) { time = _time; }
 
 		// TODO add high dimensions if needed
-		std::vector<double> getDomainShape() { return domainShape; }
+		std::vector<double> getDomainShape() const { return domainShape; }
 
 	private:
 		bool set = false;
@@ -156,6 +85,8 @@ class Parameters
 
 		unsigned long ic_p = 0;
 		unsigned long steps = 0;
+
+		double time = 0.0;
 };
 
 #endif
