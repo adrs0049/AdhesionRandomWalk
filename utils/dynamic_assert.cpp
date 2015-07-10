5e2a553d7119e147e37584530537dbc72ee71419
Andreas Buttenschoen <andreas.buttenschoen@gmail.com>
2015-07-10 16:34:29 -0600

    add dynamic assert helpers

---
+++ utils/dynamic_assert.cpp
@@ -0,0 +1,51 @@
+
+#include "dynamic_assert.h"
+
+namespace Assert {
+
+    std::string compose(const std::string& condition, const char* file,
+                        int line, const std::string& message)
+    {
+        std::ostringstream os("");
+        os << "Assertion " << condition << " failed in (" << file << "," << line << "):" << message;
+        return os.str();
+    }
+
+    /*
+    std::string compose(const char* file, int line, const std::string& message)
+    {
+        std::ostringstream os ("(");
+        os << file << "," << line << "):" << message;
+        return os.str();
+    }
+    */
+
+    template<bool condition = level(default_level), class Except = Error>
+    void dynamic(bool assertion, const std::string& message = "Assert::dynamic failed")
+    {
+        if (!assertion || current_mode == Assert::Mode::ignore_)
+            return;
+        if (current_mode == Assert::Mode::throw_)
+            throw Except{message};
+        if (current_mode == Assert::Mode::terminate_)
+            std::terminate();
+    }
+
+    template<>
+    void dynamic<false, Error>(bool, const std::string&)
+    {}
+
+    void dynamic(bool b, const std::string& s)
+    {
+        dynamic<true, Error>(b, s);
+    }
+
+    void dynamic(bool b)
+    {
+        dynamic<true, Error>(b);
+    }
+
+
+
+} // namespace end
+
---
+++ utils/dynamic_assert.h
@@ -0,0 +1,36 @@
+
+#include <string>
+#include <iostream>
+#include <exception>
+#include <stdexcept>
+#include <sstream>
+
+#define CURRENT_LEVEL 0
+
+namespace Assert {
+
+    enum class Mode { throw_, terminate_, ignore_ };
+    constexpr Mode current_mode = Mode::throw_;
+    const int current_level = CURRENT_LEVEL;
+    constexpr int default_level = 1;
+
+    constexpr bool level(int n)
+        { return n<=current_level; }
+
+    struct Error : std::runtime_error {
+        Error(const std::string& p) : std::runtime_error(p) {}
+    };
+
+    std::string compose(const std::string& condition, const char* file,
+                        int line, const std::string& message);
+
+    template<bool condition = level(default_level), class Except = Error>
+    void dynamic(bool assertion, const std::string& message = "Assert::dynamic failed");
+
+	template<>
+    void dynamic<false, Error>(bool, const std::string&);
+    void dynamic(bool b, const std::string& s);
+    void dynamic(bool b);
+
+} // end namespace
+
