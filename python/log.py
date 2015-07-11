bcf2a07a3a4a835df289f5d62831994a6fc8fc32
Andreas Buttenschoen <andreas.buttenschoen@gmail.com>
2015-07-11 17:09:24 -0600

    move log functions for saferun outside

---
+++ python/log.py
@@ -0,0 +1,24 @@
+
+
+def log_error(dest_q, message, name):
+    dest_q.put(dict(type='error',
+                    name=name,
+                    timestamp=datetime.datetime.now(),
+                    process=os.getpid(),
+                    message=message))
+
+def log_info(dest_q, message, name):
+    dest_q.put(dict(type='info',
+                    name=name,
+                    timestamp=datetime.datetime.now(),
+                    process=os.getpid(),
+                    message=message))
+
+def log_debug(dest_q, message, name):
+    dest_q.put(dict(type='debug',
+                    name=name,
+                    timestamp=datetime.datetime.now(),
+                    process=os.getpid(),
+                    message=message))
+
+
