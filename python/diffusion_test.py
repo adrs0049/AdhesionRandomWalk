f5cd887bd85f7cbcb7bf5bdd3711337759fb53e9
Andreas Buttenschoen <andreas.buttenschoen@gmail.com>
2015-07-11 17:08:53 -0600

    add a small test for diffusion like random walks

---
+++ python/diffusion_test.py
@@ -0,0 +1,25 @@
+
+import simulator as s
+from randomWalk_db import *
+from models import *
+from adhesion_1d import *
+
+import numpy as np
+
+if __name__ == '__main__':
+
+    # TODO read this from an XML file
+    param = dict(DomainSize=10, DomainN=20,
+                 diffusion_coeff=1.0, drift_coeff=0,
+                 R=1.0, omega_type=1, omega_p=0.42, g_type=1,
+                 u0=0.8, bcs='pp', ic_type=s.IC_TYPE_DELTA, ic_p=0.1)
+
+    player = Player(param)
+
+    player.runSimulations(np.arange(0.00001,1.0,0.25), 1000)
+
+    player.setNewDB()
+
+    player.doHistogram(Compare=True)
+
+
