38119f84ebe378ea9a1cf376f4df8e582cff64ae
Andreas Buttenschoen <andreas.buttenschoen@gmail.com>
2015-07-10 21:50:02 -0600

    update event testing script

--- python/event_test.py
+++ python/event_test.py
@@ -1,14 +1,23 @@
 import simulator as s
+import numpy as np
 
 EventCalled = False
 
 def f(*args, **kwargs):
-    print(args[0])
-    print("HELLO CALLBACK")
+    print('HELLO CALLBACK')
+    #print(args[0])
+    data = kwargs
+    print('kwargs_type=',type(data))
+    print('time=', np.around(data["time"], decimals=3),
+          ' steps=', data["steps"], ' states=', data["states"])
+
+    #print('type=', type(args[0]))
 
 if __name__ == '__main__':
     times = s.DVector([0.1,0.2,0.3])
-    p = s.Parameters(10,0.1,times,1)
+    print('times=', times)
+    domain = s.DVector([-5.0, 5.0])
+    p = s.Parameters(domain,0.1,times,1)
     p.setSensingRadius(1.0)
     p.setDiffusion(1.0)
     p.setDrift(1.0)
