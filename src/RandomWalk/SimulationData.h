7a6c53c784486dc728f7032d11eea814b1451cf5
Andreas Buttenschoen <andreas.buttenschoen@gmail.com>
2015-07-11 17:42:18 -0600

    use aligned vector

--- src/RandomWalk/SimulationData.h
+++ src/RandomWalk/SimulationData.h
@@ -1,18 +1,19 @@
 #ifndef SIMULATION_DATA_H
 #define SIMULATION_DATA_H
 
-#include <vector>
+#include "vector.h"
 
 struct SimulationData
 {
-	typedef std::vector<unsigned int> data_type;
+	typedef AligndVector<unsigned int> data_type;
+	//typedef std::vector<unsigned int> data_type;
 
 	SimulationData() = delete;
 	SimulationData(data_type& _states, unsigned long _steps, double _time)
 		: states(_states), steps(_steps), time(_time)
 	{}
 
-	std::vector<unsigned int>& states;
+	data_type& states;
 	unsigned long steps;
 	double time;
 };
