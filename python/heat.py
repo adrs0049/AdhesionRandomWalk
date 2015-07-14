7e7d07737dd35427bb0a1843acfef9b8be1ddffd
Andreas Buttenschoen <andreas.buttenschoen@gmail.com>
2015-07-13 22:44:25 -0600

    Solve the simple drift equation as well

--- python/heat.py
+++ python/heat.py
@@ -3,19 +3,25 @@ import matplotlib.pyplot as plt
 
 class FFTHeat1D_test(object):
 
-        def __init__(self, u, dt, L):
+        def __init__(self, u, dt, L, D, c=0.0):
             self.ux = u
             self.L = L
             self.uk = self.compute_to_k(u)
             self.n = len(self.uk)
             self.dt = dt
+            self.c = c
+            self.D = D
             self.e = self.get_derivConst()
 
-            print('dt=', dt, ' n=', self.n, ' L=', self.L)
+            print('dt=', dt, 'D=', self.D, 'c=', self.c, ' n=', \
+                  self.n, ' L=', self.L)
 
         def get_derivConst(self):
             kVec = np.arange(-self.n/2, self.n/2)
-            derivConst = -(2.0*np.pi*kVec / self.L)**2 * self.dt
+            derivConst = ( - self.D * (2.0*np.pi*kVec / self.L)**2 \
+                           - self.c * (2.0*np.pi*kVec / self.L)*1j \
+                          ) * self.dt
+            #return derivConst
             return np.exp(derivConst)
 
         def compute_to_k(self, u):
@@ -39,10 +45,10 @@ def map(a, b, x):
 
 def test():
 
-    D = 10.0
+    D = 1.0
     t = 0.1
     N = 2**10
-    domainSize = 3.0
+    domainSize = 1.0
 
     # actual domain
     x = np.arange(0, domainSize, domainSize/N)
@@ -55,7 +61,39 @@ def test():
     # initial condition
     u = np.sin(2.0 * np.pi * x / domainSize)
 
-    solver = FFTHeat1D_test(u, D * t, domainSize)
+    #solver = FFTHeat1D_test(u, D * t, domainSize)
+    solver = FFTHeat1D_test(u, t, domainSize, D)
+    solver.time_step()
+
+    u = solver.get_x()
+
+    plt.plot(x, u, color='k')
+    plt.plot(x, u_exact, color='g')
+
+    print('ERROR=', np.linalg.norm(u-u_exact))
+
+    plt.show()
+
+def test_drift():
+
+    D = 1.0
+    c = 2.0
+    t = 0.1
+    N = 2**10
+    domainSize = 1.0
+
+    # actual domain
+    x = np.arange(0, domainSize, domainSize/N)
+    u_exact = np.exp(-(2.0*np.pi / domainSize)**2 * D * t) \
+            * np.sin(2.0 * np.pi * (x - c *t) / domainSize)
+
+    # map everything to [0, 1]
+    x_solver = np.arange(0.0, 1.0, 1.0/N)
+
+    # initial condition
+    u = np.sin(2.0 * np.pi * x / domainSize)
+
+    solver = FFTHeat1D_test(u, t, domainSize, D, c)
     solver.time_step()
 
     u = solver.get_x()
