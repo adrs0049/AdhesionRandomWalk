a1bed28202547559bc335d78fc7838b772afe1ed
Andreas Buttenschoen <andreas.buttenschoen@gmail.com>
2015-07-16 20:57:22 -0600

    add null ptr deref exception

--- utils/exceptions.h
+++ utils/exceptions.h
@@ -3,11 +3,23 @@
 
 #include <exception>
 #include <stdexcept>
+#include <string>
 
 class NotImplementedException : public std::logic_error
 {	public:
 		using std::logic_error::logic_error;
 
-		virtual const char * what() const throw()
-			{ return "Function not yet implemented."; }
+		NotImplementedException(const std::string & msg)
+			: logic_error("Function " + msg + " not implemented")
+		{}
+};
+
+class NullPtrDereference : public std::runtime_error
+{
+	public:
+		using std::runtime_error::runtime_error;
+
+		NullPtrDereference(const std::string & msg)
+			: runtime_error("Pointer " + msg + " is NULL!")
+		{}
 };
